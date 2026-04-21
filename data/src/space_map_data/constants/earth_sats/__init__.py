fc10d8123f2051c0c829a3aa2e46eacfc76136c8
Julie Dujardin <julie@dujardin.pw>
2026-04-21 23:39:11 +0200

    feat(wikidata): download sat buses too

--- data/src/space_map_data/constants/earth_sats/__init__.py
+++ data/src/space_map_data/constants/earth_sats/__init__.py
@@ -1,6 +1,7 @@
 from space_map_data.constants.earth_sats.constellations import CONSTELLATIONS
 from space_map_data.constants.earth_sats.launch_sites import LAUNCH_SITES
 from space_map_data.constants.earth_sats.operators import OPERATORS
+from space_map_data.constants.earth_sats.satellite_models import SATELLITE_BUSES
 
 
 def all_wikidata_qids() -> set[str]:
@@ -13,6 +14,9 @@ def all_wikidata_qids() -> set[str]:
         if spec.wikidata_qid is not None:
             qids.add(spec.wikidata_qid)
     for spec in LAUNCH_SITES:
+        if spec.wikidata_qid is not None:
+            qids.add(spec.wikidata_qid)
+    for spec in SATELLITE_BUSES:
         if spec.wikidata_qid is not None:
             qids.add(spec.wikidata_qid)
     return qids
