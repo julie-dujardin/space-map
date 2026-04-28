3fba16b7bddad01d1557213c2f4e2d18e5cbde8f
Julie Dujardin <julie@dujardin.pw>
2026-04-29 00:06:02 +0200

    feat(frontend): proper favicons

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import faviconLight from '$lib/assets/favicon-light.svg';
-	import faviconDark from '$lib/assets/favicon-dark.svg';
 	import { getLocale, getTextDirection } from '$lib/paraglide/runtime.js';
 	import Sonner from '$lib/components/ui/sonner/sonner.svelte';
 
@@ -13,11 +11,6 @@
 		document.documentElement.dir = getTextDirection(locale);
 	});
 </script>
-
-<svelte:head>
-	<link rel="icon" media="(prefers-color-scheme: dark)" href={faviconDark} />
-	<link rel="icon" media="(prefers-color-scheme: light)" href={faviconLight} />
-</svelte:head>
 
 <Sonner position="top-right" />
 {@render children()}
