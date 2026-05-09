77e719f4fad71cd72ad24d1e0762c08c4d3340a6
Julie Dujardin <julie@dujardin.pw>
2026-05-09 21:18:56 +0200

    feat: update domain

--- frontend/src/lib/fetch/data-base.ts
+++ frontend/src/lib/fetch/data-base.ts
@@ -1,3 +1,3 @@
 import { env } from '$env/dynamic/public';
 
-export const DATA_BASE = env.PUBLIC_DATA_URL || 'https://static.space-map.net';
+export const DATA_BASE = env.PUBLIC_DATA_URL || 'https://static.spacemap.co';
