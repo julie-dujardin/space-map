d77328dfa8fea4c904c3f176a1740dbd5d7ebaed
Julie Dujardin <julie@dujardin.pw>
2026-05-10 14:32:53 +0200

    data: promote extrasolar objects

--- data/src/space_map_data/constants/promoted.py
+++ data/src/space_map_data/constants/promoted.py
@@ -94,7 +94,10 @@ PROMOTED_EXTRA_IDS: frozenset[str] = frozenset(
         "spkid-20101955",  # 101955 Bennu
         "spkid-20162173",  # 162173 Ryugu
         "spkid-20486958",  # 486958 Arrokoth
+        # Interstellar objects
         "spkid-3788040",  # 1I/ʻOumuamua
+        "spkid-1003639"  # C/2019 Q4 (Borisov)
+        "spkid-1004083"  # C/2025 N1 (ATLAS)
         # Comets
         "spkid-1000036",  # 1P/Halley
         "spkid-1000025",  # 2P/Encke
